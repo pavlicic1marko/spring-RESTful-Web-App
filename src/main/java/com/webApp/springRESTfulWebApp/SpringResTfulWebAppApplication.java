add1fed946e7d17ddd513d4bd25dd917201cb7d3
pavlicic1marko <pavlicic1marko@gmail.com>
2020-01-09 16:29:02 +0100

    Initial commit

---
+++ src/main/java/com/webApp/springRESTfulWebApp/SpringResTfulWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.webApp.springRESTfulWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResTfulWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResTfulWebAppApplication.class, args);
+	}
+
+}
