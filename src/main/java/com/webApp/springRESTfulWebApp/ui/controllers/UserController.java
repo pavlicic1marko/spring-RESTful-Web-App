76ae4f33808604c84d26a2e3a588e01027dbaa43
pavlicic1marko <pavlicic1marko@gmail.com>
2020-01-14 14:39:02 +0100

    --adding swagger description for UserController Endpoints

--- src/main/java/com/webApp/springRESTfulWebApp/ui/controllers/UserController.java
+++ src/main/java/com/webApp/springRESTfulWebApp/ui/controllers/UserController.java
@@ -6,6 +6,7 @@ import com.webApp.springRESTfulWebApp.model.response.UserInformationResponseMode
 import com.webApp.springRESTfulWebApp.service.implementation.UserServiceImplementation;
 import com.webApp.springRESTfulWebApp.shared.Utils;
 import data.transfer.objects.UserDto;
+import io.swagger.annotations.ApiOperation;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -25,6 +26,7 @@ public class UserController {
     private ModelMapper modelMapper = new ModelMapper();
 
 
+    @ApiOperation(value = "Get information for multiple users Endpoint")
     @GetMapping
     public List<UserInformationResponseModel> getUsers(@RequestParam(value = "page", defaultValue = "0") int page, @RequestParam(value = "limit", defaultValue = "20") int limit) {
         List<UserInformationResponseModel> userDetailsList = new ArrayList<>();
@@ -35,6 +37,7 @@ public class UserController {
         return userDetailsList;
     }
 
+    @ApiOperation(value = "Get information for a single users Endpoint")
     @GetMapping(path = "/{userId}")
     public UserInformationResponseModel getUser(@PathVariable String userId) {
         UserDto userDto = userServiceImplementation.getUserByUserId(userId);
@@ -42,6 +45,7 @@ public class UserController {
         return returnValue;
     }
 
+    @ApiOperation(value = "Create a new user Endpoint")
     @PostMapping
     public UserInformationResponseModel createUser(@RequestBody UserInformationRequestModel userInformation) {
         Utils utils = new Utils();
@@ -54,12 +58,14 @@ public class UserController {
 
     }
 
+    @ApiOperation(value = "Delete a  user Endpoint")
     @DeleteMapping(path = "/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public UserInformationResponseModel deleteUser(@PathVariable String userId) {
         UserDto returnValue = userServiceImplementation.deleteUser(userId);
         return modelMapper.map(returnValue, UserInformationResponseModel.class);
     }
 
+    @ApiOperation(value = "Update user information Endpoint")
     @PutMapping(path = "/{userId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public UserInformationResponseModel updateUser(@PathVariable String userId, @RequestBody UserInformationRequestModel userInformation) {
         Utils utils = new Utils();
