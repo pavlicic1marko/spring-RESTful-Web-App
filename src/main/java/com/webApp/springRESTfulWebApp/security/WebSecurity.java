72e94ee8112abb41e5dd36200e297fd90b2d51e8
pavlicic1marko <pavlicic1marko@gmail.com>
2020-01-16 22:22:55 +0100

    --making all swagger urls public

--- src/main/java/com/webApp/springRESTfulWebApp/security/WebSecurity.java
+++ src/main/java/com/webApp/springRESTfulWebApp/security/WebSecurity.java
@@ -23,6 +23,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
+                .antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
                 .anyRequest().authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
     }
 
