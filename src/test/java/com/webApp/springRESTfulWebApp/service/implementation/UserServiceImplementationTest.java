a226ed11cb3df2b15485ee9e61b71af76f73be10
pavlicic1marko <pavlicic1marko@gmail.com>
2020-01-23 12:25:03 +0100

    --adding JUnit tests for UserController.getUser()

---
+++ src/test/java/com/webApp/springRESTfulWebApp/service/implementation/UserServiceImplementationTest.java
@@ -0,0 +1,61 @@
+package com.webApp.springRESTfulWebApp.service.implementation;
+
+import com.webApp.springRESTfulWebApp.dto.UserDto;
+import com.webApp.springRESTfulWebApp.entities.UserEntity;
+import com.webApp.springRESTfulWebApp.exceptions.customexceptions.UserServiceExceptions;
+import com.webApp.springRESTfulWebApp.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+
+public class UserServiceImplementationTest {
+
+
+    @InjectMocks
+    UserServiceImplementation userService;
+
+    @Mock
+    UserRepository userRepository;
+
+    private UserEntity userEntity;
+
+    private String email = "testemail@test.com";
+    private String firstName = "Marko";
+    private String lastName = "Pavlicic";
+
+    @BeforeEach
+    final void setUp() {
+        MockitoAnnotations.initMocks(this);
+        userEntity = new UserEntity();
+        userEntity.setEmail(email);
+        userEntity.setFirstName(firstName);
+        userEntity.setLastName(lastName);
+    }
+
+    @Test
+    final void getUser() {
+        when(userRepository.findByUserId(anyString())).thenReturn(userEntity);
+        UserDto userDto = userService.getUserByUserId("testId");
+        assertNotNull(userDto);
+        assertEquals(email, userDto.getEmail());
+        assertEquals(firstName, userDto.getFirstName());
+        assertEquals(lastName, userDto.getLastName());
+    }
+
+    @Test
+    final void getUser_testUsernameNotFoundException() {
+        when(userRepository.findByUserId(anyString())).thenReturn(null);
+        assertThrows(UserServiceExceptions.class,
+                () -> userService.getUserByUserId("testId")
+        );
+
+
+    }
+}
--- src/test/java/com/webApp/springRESTfulWebApp/ui/controllers/UserControllerTest.java
+++ src/test/java/com/webApp/springRESTfulWebApp/ui/controllers/UserControllerTest.java
@@ -1,9 +1,9 @@
 package com.webApp.springRESTfulWebApp.ui.controllers;
 
+
+import com.webApp.springRESTfulWebApp.dto.AddressDto;
 import com.webApp.springRESTfulWebApp.dto.UserDto;
-import com.webApp.springRESTfulWebApp.entities.UserEntity;
-import com.webApp.springRESTfulWebApp.exceptions.customexceptions.UserServiceExceptions;
-import com.webApp.springRESTfulWebApp.repositories.UserRepository;
+import com.webApp.springRESTfulWebApp.model.response.UserInformationResponseModel;
 import com.webApp.springRESTfulWebApp.service.implementation.UserServiceImplementation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,52 +11,75 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 
+
 public class UserControllerTest {
 
-
     @InjectMocks
-    UserServiceImplementation userService;
+    UserController userController;
 
     @Mock
-    UserRepository userRepository;
-
-    private UserEntity userEntity;
+    UserServiceImplementation userServiceImplementation;
 
     private String email = "testemail@test.com";
     private String firstName = "Marko";
     private String lastName = "Pavlicic";
+    private String userId = "testID";
+    private String password = "1234";
+
+    private String streetNumber = "12";
+    private String streetName = "1st";
+    private String city = "NY";
+    private String addressType = "work";
+    private String addressId = "testID";
+
+    private AddressDto addressDto;
+    private UserDto userdto;
+    private List<AddressDto> addressDtoList = new ArrayList<>();
 
     @BeforeEach
     final void setUp() {
         MockitoAnnotations.initMocks(this);
-        userEntity = new UserEntity();
-        userEntity.setEmail(email);
-        userEntity.setFirstName(firstName);
-        userEntity.setLastName(lastName);
+        addressDto = new AddressDto();
+        addressDto.setStreetNumber(streetNumber);
+        addressDto.setStreetName(streetName);
+        addressDto.setCity(city);
+        addressDto.setAddressType(addressType);
+        addressDto.setAddressId(addressId);
+        addressDtoList.add(0, addressDto);
+
+
+        userdto = new UserDto();
+        userdto.setAddresses(addressDtoList);
+        userdto.setFirstName(firstName);
+        userdto.setEmail(email);
+        userdto.setLastName(lastName);
+        userdto.setPassword(password);
+        userdto.setUserId(userId);
+
     }
+
 
     @Test
     final void getUser() {
-        when(userRepository.findByUserId(anyString())).thenReturn(userEntity);
-        UserDto userDto = userService.getUserByUserId("testId");
-        assertNotNull(userDto);
-        assertEquals(email, userDto.getEmail());
-        assertEquals(firstName, userDto.getFirstName());
-        assertEquals(lastName, userDto.getLastName());
-    }
-
-    @Test
-    final void getUser_testUsernameNotFoundException() {
-        when(userRepository.findByUserId(anyString())).thenReturn(null);
-        assertThrows(UserServiceExceptions.class,
-                () -> userService.getUserByUserId("testId")
-        );
-
+        when(userServiceImplementation.getUserByUserId(anyString())).thenReturn(userdto);
+        UserInformationResponseModel userInformation = userController.getUser("testId");
+        assertNotNull(userInformation);
+        assertEquals(email, userInformation.getEmail());
+        assertEquals(firstName, userInformation.getFirstName());
+        assertEquals(lastName, userInformation.getLastName());
+        assertEquals(userdto.getAddresses().size(), userInformation.getAddresses().size());
+        assertEquals(city, userInformation.getAddresses().get(0).getCity());
 
     }
+
+
 }
