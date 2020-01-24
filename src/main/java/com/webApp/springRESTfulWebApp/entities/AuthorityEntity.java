08a6b912c39b8e1f6aa007f118d28593cc2d4be2
pavlicic1marko <pavlicic1marko@gmail.com>
2020-01-24 20:14:41 +0100

    --adding AuthorityEntity.java and  fields in RoleEntity for JoinColumn

---
+++ src/main/java/com/webApp/springRESTfulWebApp/entities/AuthorityEntity.java
@@ -0,0 +1,45 @@
+package com.webApp.springRESTfulWebApp.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+
+@Entity
+@Table(name = "authorities")
+public class AuthorityEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @ManyToMany(mappedBy = "authorities")
+    private Collection<RoleEntity> roles;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Collection<RoleEntity> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<RoleEntity> roles) {
+        this.roles = roles;
+    }
+}
--- src/main/java/com/webApp/springRESTfulWebApp/entities/RoleEntity.java
+++ src/main/java/com/webApp/springRESTfulWebApp/entities/RoleEntity.java
@@ -1,4 +1,56 @@
 package com.webApp.springRESTfulWebApp.entities;
 
-public class RoleEntity {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+
+@Entity
+@Table(name = "roles")
+public class RoleEntity implements Serializable {
+
+    private static final long serialVersionUID = 8884534222345334538L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Collection<UserEntity> users;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST}, fetch = FetchType.EAGER)
+    @JoinTable(name = "roles_authorities",
+            joinColumns = @JoinColumn(name = "roles_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "authorities_id", referencedColumnName = "id"))
+    private Collection<AuthorityEntity> authorities;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Collection<UserEntity> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Collection<UserEntity> users) {
+        this.users = users;
+    }
+
+    public Collection<AuthorityEntity> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Collection<AuthorityEntity> authorities) {
+        this.authorities = authorities;
+    }
 }
