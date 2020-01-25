474ade01eb262504aa81e0c6bf0b6894d0a6d342
pavlicic1marko <pavlicic1marko@gmail.com>
2020-01-25 23:59:13 +0100

    --adding Roles functionality

--- src/main/java/com/webApp/springRESTfulWebApp/CreatingDefaultUsers.java
+++ src/main/java/com/webApp/springRESTfulWebApp/CreatingDefaultUsers.java
@@ -1,11 +1,17 @@
 package com.webApp.springRESTfulWebApp;
 
 import com.webApp.springRESTfulWebApp.entities.AuthorityEntity;
+import com.webApp.springRESTfulWebApp.entities.RoleEntity;
 import com.webApp.springRESTfulWebApp.repositories.AuthorityRepository;
+import com.webApp.springRESTfulWebApp.repositories.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 @Component
 public class CreatingDefaultUsers {
@@ -13,13 +19,21 @@ public class CreatingDefaultUsers {
     @Autowired
     AuthorityRepository authorityRepository;
 
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Transactional
     @EventListener
     public void onApplicationReadyEvent(ApplicationReadyEvent event) {
         AuthorityEntity readAuthority = createAuthority("READ_AUTHORITY");
         AuthorityEntity writeAuthority = createAuthority("WRITE_AUTHORITY");
         AuthorityEntity deleteAuthority = createAuthority("DELETE_AUTHORITY");
+
+        createRole("ROLE_USER", Arrays.asList(readAuthority, writeAuthority));
+        createRole("ROLE_ADMIN", Arrays.asList(readAuthority, writeAuthority, deleteAuthority));
     }
 
+    @Transactional
     private AuthorityEntity createAuthority(String name) {
         AuthorityEntity authority = authorityRepository.findByName(name);
         if (authority == null) {
@@ -27,6 +41,17 @@ public class CreatingDefaultUsers {
             authorityRepository.save(authority);
         }
         return authority;
+    }
+
+    @Transactional
+    private RoleEntity createRole(String name, Collection<AuthorityEntity> authorities) {
+        RoleEntity role = roleRepository.findByName(name);
+        if (role == null) {
+            role = new RoleEntity(name);
+            role.setAuthorities(authorities);
+            roleRepository.save(role);
+        }
+        return role;
     }
 
 }
