21d922142065035b9d69a84ce53e1f6d78a26612
pavlicic1marko <pavlicic1marko@gmail.com>
2022-12-26 23:43:34 +0100

    commit in feature feature last

--- src/main/java/com/webApp/springRESTfulWebApp/dto/TestDto.java
+++ src/main/java/com/webApp/springRESTfulWebApp/dto/TestDto.java
@@ -1,5 +1,13 @@
 package com.webApp.springRESTfulWebApp.dto;
 
 public class TestDto {
+    int last4;
+
+    private void feature4(){
+        int rt4;
+        int test4;
+    }
+    //test4
+    int last2;
     //test1
 }
