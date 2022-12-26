4b24a2f2335df58d72e33a26b02ba10ded3eabc7
pavlicic1marko <pavlicic1marko@gmail.com>
2022-12-26 23:29:06 +0100

    commit in feature addnewmethod-12

--- src/main/java/com/webApp/springRESTfulWebApp/dto/TestDto.java
+++ src/main/java/com/webApp/springRESTfulWebApp/dto/TestDto.java
@@ -1,5 +1,12 @@
 package com.webApp.springRESTfulWebApp.dto;
 
 public class TestDto {
+    public int fff;
+    public int testNumber = 12;
+
+    public int multiply(int number){
+        int ee =2;
+        return number * ee;
+    }
     //test1
 }
